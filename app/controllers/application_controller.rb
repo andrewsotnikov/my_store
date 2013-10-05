538482565916227018e3b7d46d32df28b1c03acb
andrewsotnikov <a.sotnikov@libertyrus.ru>
2013-10-05 17:23:58 +0400

    cast 37-38

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :set_locale
   before_filter :check_admin 
   
   private
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+    
+    def default_url_options(options={})
+      { locale: I18n.locale }
+    end
   
     def render_403
       render file: "public/403.html", status: 403, layout: "application" 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 MyStore::Application.routes.draw do
   # get ':controller(/:action(/:id))(.:format)'
-  resources :items do
-    get :upvote, on: :member
-    get :expensive, on: :collection
+  scope "/:locale" do
+    resources :items do
+      get :upvote, on: :member
+      get :expensive, on: :collection
+    end
   end
   
   get 'admin/users_count' => 'admin#users_count'
